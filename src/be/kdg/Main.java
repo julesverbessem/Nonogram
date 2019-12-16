c7fb6ccd78cc53a11096fb266e411a13cbdba3c4
jules <jules.verbessem@student.kdg.be>
2019-12-16 19:51:42 +0100

    maandag versie 4 thuis

--- src/be/kdg/Main.java
+++ src/be/kdg/Main.java
@@ -3,7 +3,7 @@ package be.kdg;
 public class Main {
 
     public static void main(String[] args) {
-	    Nonogram test = new Nonogram(1, "test");
+	    Nonogram test = new Nonogram(1, "test", 10);
 	    test.toonGrid();
     }
 }
--- src/be/kdg/Nonogram.java
+++ src/be/kdg/Nonogram.java
@@ -5,32 +5,31 @@ import java.lang.reflect.Array;
 public class Nonogram {
     private int moeilijkheidsgraad;
     private String naam;
-    private String[] patroon = {"X",};
+    private int grootte;
+    private Vakje[][] patroon;
 
-    public Nonogram(int moeilijkheidsgraad, String naam) {
+    public Nonogram(int moeilijkheidsgraad, String naam, int grootte) {
         this.moeilijkheidsgraad = moeilijkheidsgraad;
         this.naam = naam;
+        this.grootte = grootte;
+        this.patroon = new Vakje[grootte][grootte];
+        for(int rij = 1; rij <= grootte; rij++){
+            for(int kolom = 1; kolom <= grootte; kolom++){
+                int r = rij;
+                int k = kolom;
+                this.patroon[r-1][k-1] = new Vakje();
+            }
+        }
         }
 
     public int getMoeilijkheidsgraad() {
         return moeilijkheidsgraad;
     }
 
-    public void setMoeilijkheidsgraad(int moeilijkheidsgraad) {
-        this.moeilijkheidsgraad = moeilijkheidsgraad;
-    }
-
     public String getNaam() {
         return naam;
     }
 
-    public String[] getPatroon() {
-        return patroon;
-    }
-
-    public void setNaam(String naam) {
-        this.naam = naam;
-    }
 
     public void controlleren() {
 
@@ -40,24 +39,21 @@ public class Nonogram {
         return "Gefeliciteerd, heeft het nonogram " + this.naam + " afgewerkt";
     }
 
-    public void toonGrid() {
+    public void toonGrid(){
+        StringBuilder builder = new StringBuilder();
+        for(int rij = 1; rij < grootte; rij++){
+            for(int kolom = 1; kolom < grootte; kolom++){
+                int r = rij;
+                int k = kolom;
+                builder.append(patroon[r-1][k-1].getWaarde()).append(" ");
+                if(kolom%grootte==0){
 
-        StringBuilder grid = new StringBuilder();
-        for(int i = 1; i < 11;i++) {
-            patroon[i] = new Vakje().getWaarde();
+                    builder.append(patroon[r-1][k-1].getWaarde()).append(" ").append("\n");
+                }
+            }
         }
-        for(int i = 1; i < 11;i++) {
-            if(i%10== 0){
-                grid.append(patroon[i-1]).append(" ").append("\n");
-            }else{
-                grid.append(patroon[i-1]).append(" ");
-        }
-            System.out.println(grid.append("\n").toString());
-
-        }
-
-
-
+        System.out.println(builder.append("\n").toString());
     }
+
 
 }
