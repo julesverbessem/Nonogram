4f4af020746d25539df3bd50486d9cf5107f3577
jules <jules.verbessem@student.kdg.be>
2019-12-19 20:42:35 +0100

    donderdag 19/12: jules deel 1

--- src/be/kdg/Gebruiker.java
+++ src/be/kdg/Gebruiker.java
@@ -4,13 +4,14 @@ public class Gebruiker {
     private String gebruikersnaam;
     private String passwoord;
     public Spel opgeslagenSpel;
+    private int level;
 
     public Gebruiker(String gebruikersnaam, String passwoord) {
         this.gebruikersnaam = gebruikersnaam;
         this.passwoord = passwoord;
+        this.opgeslagenSpel = new Spel();
     }
     public Gebruiker(){}
-
 
     public String getGebruikersnaam() {
         return gebruikersnaam;
@@ -36,5 +37,11 @@ public class Gebruiker {
         this.opgeslagenSpel = opgeslagenSpel;
     }
 
+    public int getLevel() {
+        return level;
+    }
 
+    public void setLevel(int level) {
+        this.level = level;
+    }
 }
